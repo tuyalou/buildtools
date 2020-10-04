790fd9d8e13a7a8cb2ff2212e9b446e8ae938a91
tuyalou <63755131+tuyalou@users.noreply.github.com>
2020-10-04 15:39:20 -0500

    Update JenkinsBuilder.groovy

--- JenkinsBuilder.groovy
+++ JenkinsBuilder.groovy
@@ -2,33 +2,12 @@
   def repositoryName = "${JOB_NAME}"
   def environment = ""
   def gitCommitHash = ""
-  def registry = ""
 
   def branch = "${scm.branches[0].name}".replaceAll(/^\*\//, '')
   if (branch =~ '^v[0-9].[0-9]' || branch =~ '^v[0-9][0-9].[0-9]' ) {
         // if Application release or branch starts with v* example v0.1 will be deployed to prod
         environment = 'prod' 
         repositoryName = repositoryName + '-prod'
-
-  } else if (branch.contains('dev-feature')) {
-        // if branch name contains dev-feature then the deploy will be deployed to dev environment 
-        environment = 'dev' 
-        repositoryName = repositoryName + '-dev-feature'
-
-  } else if (branch.contains('qa-feature')) {
-        // if branch name contains q-feature then the deploy will be deployed to qa environment
-        repositoryName = repositoryName + '-qa-feature'
-        environment = 'qa' 
-
-  } else if (branch.contains('PR')) {
-        // PR means Pull requests all PR will be deployed to test namespace 
-        repositoryName = repositoryName + '-pr-feature'
-        environment = 'test' 
-
-  } else if (branch == 'master') {
-        // If branch is master it will be deployed to stage environment 
-        environment = 'stage' 
-        repositoryName = repositoryName + '-stage'
   }
 
 def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
@@ -84,7 +63,7 @@ def slavePodTemplate = """
         dir('Docker/') {
           stage("Docker Build") {
               container("docker") {
-                  dockerImage = docker.build(repositoryName, "--build-arg environment=${environment.toLowerCase()} .")
+                  dockerImage = docker.build(repositoryName)
               }
           }
           stage("Docker Login") {
