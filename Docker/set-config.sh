59348700fa2e1ff6ae981b0a6be252e7e918f135
tuyalou <63755131+tuyalou@users.noreply.github.com>
2023-01-19 15:53:29 -0600

    Update set-config.sh

--- Docker/set-config.sh
+++ Docker/set-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-kubectl config set-cluster fuchicorp-cluster --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-kubectl config set-context fuchicorp-context --cluster=fuchicorp-cluster
-kubectl config set-credentials fuchicorp-user --token="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
-kubectl config set-context fuchicorp-context --user=fuchicorp-user
-kubectl config use-context fuchicorp-context
+kubectl config set-cluster  ada-cluster --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+kubectl config set-context ada-context --cluster=ada-cluster
+kubectl config set-credentials ada-user --token="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
+kubectl config set-context ada-context --user=ada-user
+kubectl config use-context ada-context
