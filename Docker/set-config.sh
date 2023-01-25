38a1cd0a662cf96c4ba8146499c9ea63d496bf2f
tuyalou <63755131+tuyalou@users.noreply.github.com>
2023-01-24 18:32:37 -0600

    Update set-config.sh

--- Docker/set-config.sh
+++ Docker/set-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 kubectl config set-cluster  tiffany-cluster --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-kubectl config set-context tiffany-context --cluster=ada-cluster
+kubectl config set-context tiffany-context --cluster=tiffany-cluster
 kubectl config set-credentials tiffany-user --token="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
 kubectl config set-context tiffany-context --user=tiffany-user
 kubectl config use-context tiffany-context
